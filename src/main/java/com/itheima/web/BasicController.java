3317d4181a0cea8ac233ce1265d5d2e71d33e76a
ybyun <ybyun@126.com>
2018-11-15 17:43:13 +0800

    提交左侧输出

--- src/main/java/com/itheima/web/BasicController.java
+++ src/main/java/com/itheima/web/BasicController.java
@@ -6,5 +6,6 @@ public class BasicController {
     public static void main(String[] args) {
         System.out.println("right");
         System.out.println("left");
+        System.out.println("aaaaa");
     }
 }
